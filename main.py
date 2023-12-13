9d64457e14212f2e4194c195923937dbc70ffe92
lujiangCassie <lu.jiang201845@gmail.com>
2023-12-13 09:37:47 -0800

    modify rf_config file path

--- main.py
+++ main.py
@@ -94,11 +94,10 @@ def go(config: DictConfig):
         if "train_random_forest" in active_steps:
 
             # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
+            rf_config = os.path.join(os.getcwd(), "rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            print(rf_config)
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
             _ = mlflow.run(
